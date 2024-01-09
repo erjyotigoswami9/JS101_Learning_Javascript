8da3be0d18fcc50b0cf52eb9fca8c0f2b762e523
erjyotigoswami9 <erjyotigoswami9@gmail.com>
2024-01-09 22:14:49 +0530

    Program3 added

---
+++ Introduction_to_Javascript_Program4.js
@@ -0,0 +1,32 @@
+var a1="🆁 🅴 🅿 🅾 🆁 🆃  🅲 🅰 🆁 🅳 "
+var a2=" 𝕊𝕔𝕙𝕠𝕠𝕝 𝕊𝟙"
+var name=" 𝙉𝙖𝙢𝙚 𝘼1 "
+var grade=" 𝗅 𝕲𝖗𝖆𝖉𝖊 5 "
+var section_of_student=" 𝗅 S͛eͤcͨᴛⷮiͥoͦn dͩ "
+var rollno=" 𝗅 ℝ𝕠𝕝𝕝 ℕ𝕠-𝟙𝟝 "
+console.log("")
+console.log("____________________________________________")
+console.log("")
+console.log(a1+"  OF  "+a2)
+console.log("")
+console.log("_____________________________________________")
+console.log("")
+console.log(name+grade+section_of_student+rollno)
+console.log("")
+console.log("_____________________________________________")
+console.log("")
+console.log("Ⓜ ⓐ ⓡ ⓚ ⓢ  Ⓢ ⓒ ⓞ ⓡ ⓔ ⓓ")
+console.log("")
+console.log("______________________________________________")
+console.log("")
+var marks1=" ᔆᶜⁱᵉⁿᶜᵉ  ==>  40/50  "
+var marks2=" Mⷨaͣᴛⷮhͪs͛    ==>  35/50 "
+var marks3=" Eͤngliͥs͛hͪ  ==> 42/50  "
+console.log(marks1)
+console.log("")
+console.log(marks2)
+console.log("")
+console.log(marks3)
+console.log("")
+console.log("__________________________________________")
+console.log(" _̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_̷_")
