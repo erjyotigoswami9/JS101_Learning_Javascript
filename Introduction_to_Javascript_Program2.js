98a39af33b8210902ad174357bf8ddec6c094387
erjyotigoswami9 <erjyotigoswami9@gmail.com>
2024-01-09 22:12:34 +0530

    Program2 added

---
+++ Introduction_to_Javascript_Program2.js
@@ -0,0 +1,13 @@
+var name="Jyoti"
+console.log(name)
+name="Bhavesh Goswami"
+console.log(name)
+name="Bhawana Goswami"
+console.log(name)
+//console.log("")
+//var myname="Jyoti Goswami"
+//console.log(myname)
+//var fname="Bhavesh Goswami"
+//console.log(fname)
+//var mname="Bhawana Goswami"
+//console.log(mname)
